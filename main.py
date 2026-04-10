1ba0c932d363d97edfa37bd2b1bfb11c56f6731d
JDuwarahavidyan <duwarahavidyan_j_e22@engug.ruh.ac.lk>
2026-04-10 23:34:57 +0530

    feat: add mcp-server-time to configuration and project dependencies

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-def main():
-    print("Hello from 17-mcp!")
+from mcp_servers.server_config import config
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain_groq import ChatGroq
+from langchain.agents import create_agent
 
+from dotenv import load_dotenv
+
+_ = load_dotenv()
+
+import asyncio
+
+async def main():
+    
+    llm = ChatGroq(model="llama-3.3-70b-versatile")
+    
+    # Create MCP client
+    client = MultiServerMCPClient(config)
+    
+    
+    tools = await client.get_tools()
+    
+    tool_info = [tool.name for tool in tools]
+    
+    print(f"Available Tools: {tool_info}")
+    
+    # Create agent
+    agent = create_agent(
+        model=llm,
+        tools=tools,
+    )
+    
+    # Run agent
+    response = await agent.ainvoke({"messages": [{"role": "user", "content": "list allowed directories"}]})
+    
+    print(response['messages'][-1].content)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
---
+++ mcp_servers/server_config.py
@@ -0,0 +1,41 @@
+config = {  
+    "filesystem": {
+        "command": "npx",
+        "args": [
+            "-y",
+            "@modelcontextprotocol/server-filesystem",
+            r"D:\WSO2 Learnings"
+        ],
+        "transport": "stdio"
+    },
+    
+    "math": {
+        "command": "uv",
+        "args": ["run", "mcp_servers/math_server.py"],
+        "transport":"stdio"
+    },
+    
+    "research": {
+        "url": "http://localhost:8001/sse",
+        "transport": "sse"
+    },
+    
+    "internet_search": {
+        "url": "http://localhost:8002/mcp",
+        "transport": "http"
+    },
+    
+    "fetch": {
+        "transport": "stdio",
+        "command": "uvx",
+        "args": ["mcp-server-fetch"],
+    },
+    
+    "time": {
+      "command": "python",
+      "args": ["-m", "mcp_server_time"],
+       "transport": "stdio"
+    }
+    
+}
+  
